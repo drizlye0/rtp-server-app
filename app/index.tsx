4033b535a6c37503eec9b45067f9a3c660f75215
drizlye0 <danielkastillo03@gmail.com>
2026-03-22 00:49:30 -0500

    first commit

---
+++ app/index.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native"
+
+export const HomeScreen = () => {
+  return (
+    <View>
+      <Text>Hello world</Text>
+    </View>
+  )
+}
+
+export default HomeScreen;
--- app/modal.tsx
+++ app/modal.tsx
@@ -1,17 +1,10 @@
-import { Link } from 'expo-router';
-import { StyleSheet } from 'react-native';
-
-import { ThemedText } from '@/components/themed-text';
-import { ThemedView } from '@/components/themed-view';
+import { StyleSheet, View, Text } from 'react-native';
 
 export default function ModalScreen() {
   return (
-    <ThemedView style={styles.container}>
-      <ThemedText type="title">This is a modal</ThemedText>
-      <Link href="/" dismissTo style={styles.link}>
-        <ThemedText type="link">Go to home screen</ThemedText>
-      </Link>
-    </ThemedView>
+    <View style={styles.container}>
+      <Text>this is a modal screen</Text>
+    </View>
   );
 }
 
